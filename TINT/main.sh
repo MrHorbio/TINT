ed75575db0515dd609ec81a6d6205b80d2e3aa5c
MrHorbio <hrofficial62@gmail.com>
2025-04-15 04:56:27 +0000

    add ui

--- TINT/main.sh
+++ TINT/main.sh
@@ -3,10 +3,13 @@
 
 
 cat << "EOF"
-████████╗██╗███╗░░██╗████████╗
-╚══██╔══╝██║████╗░██║╚══██╔══╝
-░░░██║░░░██║██╔██╗██║░░░██║░░░
-░░░██║░░░██║██║╚████║░░░██║░░░
-░░░██║░░░██║██║░╚███║░░░██║░░░
-░░░╚═╝░░░╚═╝╚═╝░░╚══╝░░░╚═╝░░░
+                                    ████████╗██╗███╗░░██╗████████╗
+                                    ╚══██╔══╝██║████╗░██║╚══██╔══╝
+                                    ░░░██║░░░██║██╔██╗██║░░░██║░░░
+                                    ░░░██║░░░██║██║╚████║░░░██║░░░
+                                    ░░░██║░░░██║██║░╚███║░░░██║░░░
+                                    ░░░╚═╝░░░╚═╝╚═╝░░╚══╝░░░╚═╝░░░
 EOF
+echo -e "                                  Developed By: Ankush Kumar Rajput\n      "
+echo -e "                                           Discription           "
+echo -e " TINT stands for 'Target information gathering tool'. It helps to automate your task and helps to gather informtaion from internet.\n Features:\n + Subdomains Enumeration           + Port Scanning\n + Finding Redirections             + Filter subdomains based on status code\n + Dns resolver                     + Reverse DNS\n + WHOIS Lookup \n  " 
